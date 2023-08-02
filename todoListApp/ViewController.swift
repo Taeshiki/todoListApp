4da24729186d730953c80efb35907bf56f2ec4cc
Taeshiki <durudurutta@gmail.com>
2023-08-02 20:51:41 +0900

    스토리보드를 이용한 버튼생성 및 이미지 추가

--- todoListApp/ViewController.swift
+++ todoListApp/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imgView.image = UIImage(named: "todoImg.png")
     }
-
 
 }
 
